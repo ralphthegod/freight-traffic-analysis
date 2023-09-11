d7f2855656e793fc0caab24cabb77919620afcd8
Vincenzo Tramo <vv.tramo@gmail.com>
2023-09-11 19:22:04 +0200

    feat: add StreetResource

---
+++ quarkus/fta/src/main/java/fta/StreetResource.java
@@ -0,0 +1,37 @@
+package fta;
+
+import fta.data.StreetsPage;
+import fta.data.StreetTrafficReportsPage;
+import fta.service.StreetService;
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.graphql.DefaultValue;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
+
+import java.time.ZonedDateTime;
+
+@GraphQLApi
+public class StreetResource {
+
+    @Inject
+    StreetService streetService;
+
+    @Query
+    public Uni<StreetsPage> getStreets(
+        final int first,
+        @DefaultValue(value = "0") final Integer offset
+    ) {
+        return streetService.getStreets(first, offset);
+    }
+
+    @Query
+    public Uni<StreetTrafficReportsPage> getStreetTrafficReports(
+        final int first,
+        final int offset,
+        final ZonedDateTime start,
+        final ZonedDateTime end
+    ) {
+        return streetService.streetTrafficReport(first, offset, start, end);
+    }
+}
