fa4d7ae351a2251525057346f0e143496960b6c4
Vincenzo Tramo <vv.tramo@gmail.com>
2023-09-11 19:18:43 +0200

    feat: add TimestampEntity

--- quarkus/fta/src/main/java/fta/entity/TimestampEntity.java
+++ quarkus/fta/src/main/java/fta/entity/TimestampEntity.java
@@ -1,16 +1,17 @@
 package fta.entity;
 
-import org.neo4j.ogm.annotation.NodeEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@NodeEntity("Timestamp")
+@Getter
+@Setter
 public class TimestampEntity {
-    int day;
-    int hour;
-    int minute;
-    int month;
-    int year;
-
-    List<StreetEntity> streets;
+    private int day;
+    private int hour;
+    private int minute;
+    private int month;
+    private int year;
+    private List<StreetEntity> streets;
 }
