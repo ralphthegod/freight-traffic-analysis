c5c00eada6dca0c6200e9aa1ac26948cf7cc78db
Vincenzo Tramo <vv.tramo@gmail.com>
2023-09-11 19:16:31 +0200

    feat: add Neo4jEntityFields

---
+++ quarkus/fta/src/main/java/fta/repository/Neo4jEntityFields.java
@@ -0,0 +1,9 @@
+package fta.repository;
+
+public abstract class Neo4jEntityFields {
+    public static final String STREET_ID_FIELD = "id";
+    public static final String STREET_DATASET_FIELD = "dataset";
+    public static final String SEGMENT_START_COORD_FIELD = "start_coordinates";
+    public static final String SEGMENT_END_COORD_FIELD = "end_coordinates";
+    public static final String MADE_OF_ORDER_FIELD = "order";
+}
