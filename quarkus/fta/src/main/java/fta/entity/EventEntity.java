4bbdc12fce3183663ac73531f2cb2985c268a314
Vincenzo Tramo <vv.tramo@gmail.com>
2023-09-15 00:55:21 +0200

    feat: add mongodb constants

--- quarkus/fta/src/main/java/fta/entity/EventEntity.java
+++ quarkus/fta/src/main/java/fta/entity/EventEntity.java
@@ -1,6 +1,7 @@
 package fta.entity;
 
 public class EventEntity {
+    public static final String COLLECTION_NAME = "traffic_events";
     StreetEntity street;
     TimestampEntity timestamp;
     double traffic;
