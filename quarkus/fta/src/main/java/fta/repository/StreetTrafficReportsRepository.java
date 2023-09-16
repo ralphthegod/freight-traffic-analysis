f2d4e2e1a68ff8a0e67b0ae4bc9817d8e49c975b
Vincenzo Tramo <vv.tramo@gmail.com>
2023-09-16 18:21:34 +0200

    fix: make persistent all street reports

--- quarkus/fta/src/main/java/fta/repository/StreetTrafficReportsRepository.java
+++ quarkus/fta/src/main/java/fta/repository/StreetTrafficReportsRepository.java
@@ -8,7 +8,6 @@ import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import io.quarkus.mongodb.reactive.ReactiveMongoClient;
 import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
 import io.quarkus.mongodb.reactive.ReactiveMongoDatabase;
-import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -16,7 +15,6 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.neo4j.driver.Driver;
 
-import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -40,11 +38,9 @@ public class StreetTrafficReportsRepository implements PanacheMongoRepository<St
         return findByDatasetBetween(dataset, first, offset, start, end)
             .flatMap(optionalStreetTrafficReports -> optionalStreetTrafficReports.isPresent()
                 ? Uni.createFrom().item(optionalStreetTrafficReports.get().getStreetTrafficReports())
-                : buildStreetTrafficReports(dataset, first, offset, start, end)
-                    .collect()
-                    .asList()
+                : computeStreetTrafficReports(dataset, first, offset, start, end)
                     .onItem()
-                    .invoke(trafficReports -> persistTrafficReports(trafficReports, start, end, dataset)));
+                    .call(() -> computeAndPersistTrafficReports(start, end, dataset)));
     }
 
     public Uni<Optional<StreetTrafficReportsEntity>> findByDatasetBetween(
@@ -79,31 +75,26 @@ public class StreetTrafficReportsRepository implements PanacheMongoRepository<St
         return List.of(match, project);
     }
 
-    public void persistTrafficReports(
-        final List<StreetTrafficReport> streetTrafficReports,
+    private Uni<Void> computeAndPersistTrafficReports(
         final ZonedDateTime start,
         final ZonedDateTime end,
         final String dataset
     ) {
-        persistTrafficReports(streetTrafficReports, start.toInstant(), end.toInstant(), dataset);
-    }
-
-    public void persistTrafficReports(
-        final List<StreetTrafficReport> streetTrafficReports,
-        final Instant start,
-        final Instant end,
-        final String dataset
-    ) {
         Log.infof("Persist traffic reports. Start: %s, End: %s.", start, end);
-        final StreetTrafficReportsEntity streetTrafficReportsEntity = new StreetTrafficReportsEntity();
-        streetTrafficReportsEntity.setStreetTrafficReports(streetTrafficReports);
-        streetTrafficReportsEntity.setStart(start);
-        streetTrafficReportsEntity.setEnd(end);
-        streetTrafficReportsEntity.setDataset(dataset);
-        StreetTrafficReportsEntity.persist(streetTrafficReportsEntity);
+        computeStreetTrafficReports(dataset, start, end)
+            .subscribe()
+                .with(streetTrafficReports -> {
+                    final StreetTrafficReportsEntity streetTrafficReportsEntity = new StreetTrafficReportsEntity();
+                    streetTrafficReportsEntity.setStreetTrafficReports(streetTrafficReports);
+                    streetTrafficReportsEntity.setStart(start.toInstant());
+                    streetTrafficReportsEntity.setEnd(end.toInstant());
+                    streetTrafficReportsEntity.setDataset(dataset);
+                    StreetTrafficReportsEntity.persist(streetTrafficReportsEntity);
+                });
+        return Uni.createFrom().voidItem();
     }
 
-    public Multi<StreetTrafficReport> buildStreetTrafficReports(
+    public Uni<List<StreetTrafficReport>> computeStreetTrafficReports(
         final String dataset,
         final int first,
         final int offset,
@@ -112,7 +103,21 @@ public class StreetTrafficReportsRepository implements PanacheMongoRepository<St
     ) {
         return Neo4jReactiveSessions.executeRead(neo4jDriver, buildTrafficReportsBetweenQuery(dataset, first, offset, start, end))
             .onItem()
-            .transform(streetMapper::toStreetTrafficReport);
+            .transform(streetMapper::toStreetTrafficReport)
+            .collect()
+            .asList();
+    }
+
+    private Uni<List<StreetTrafficReport>> computeStreetTrafficReports(
+        final String dataset,
+        final ZonedDateTime start,
+        final ZonedDateTime end
+    ) {
+        return Neo4jReactiveSessions.executeRead(neo4jDriver, buildTrafficReportsBetweenQuery(dataset, start, end))
+            .onItem()
+            .transform(streetMapper::toStreetTrafficReport)
+            .collect()
+            .asList();
     }
 
     private static String buildTrafficReportsBetweenQuery(
@@ -135,5 +140,23 @@ public class StreetTrafficReportsRepository implements PanacheMongoRepository<St
             SKIP %s
             LIMIT %s
         """, start, end, dataset, offset, first);
+    }
+
+    private static String buildTrafficReportsBetweenQuery(
+        final String dataset,
+        final ZonedDateTime start,
+        final ZonedDateTime end
+    ) {
+        return String.format("""
+            MATCH p=(street:Street)-[event:HAS_EVENT]->(timestamp:Timestamp)
+            WHERE timestamp.datetime >= datetime("%s") AND timestamp.datetime <= datetime("%s") AND street.dataset = '%s'
+            RETURN street,
+                SUM(toInteger(event.traffic)) AS sumTraffic,
+                MAX(toInteger(event.traffic)) AS maxTraffic,
+                MIN(toInteger(event.traffic)) AS minTraffic,
+                AVG(toInteger(event.traffic)) AS avgTraffic,
+                AVG(toInteger(event.velocity)) AS avgVelocity,
+                MAX(toInteger(event.velocity)) AS maxVelocity
+        """, start, end, dataset);
     }
 }
