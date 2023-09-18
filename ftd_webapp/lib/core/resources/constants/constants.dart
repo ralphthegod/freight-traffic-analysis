ecc6b4d310937af77aca1c3ef556e6240217faf3
RalphTheCreator <rafdimaso@gmail.com>
2023-09-18 14:24:49 +0200

    feat: add constants

---
+++ ftd_webapp/lib/core/resources/constants/constants.dart
@@ -0,0 +1,10 @@
+
+import 'package:logger/logger.dart';
+
+const String GQLApiBaseUrl = 'http://localhost:8080/graphql';
+
+// Debug
+const bool isProduction = false;
+
+// Logs
+final logger = Logger();
