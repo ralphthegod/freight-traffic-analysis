685e22127d1a394ff0258c9653c0e926e88babb4
RalphTheCreator <rafdimaso@gmail.com>
2023-09-18 14:23:50 +0200

    feat: customize theme

---
+++ ftd_webapp/lib/config/themes/theme_data.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+
+  colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 37, 37, 56), brightness: Brightness.light,),
+);
